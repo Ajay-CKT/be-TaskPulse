4ba0aa17c65e98846c22c5b1d03bc3eff8552957
Ajay-CKT <ajayckt917@gmail.com>
2025-02-20 16:58:18 +0530

    updated cron 2

--- utils/taskScheduler.js
+++ utils/taskScheduler.js
@@ -2,21 +2,19 @@ const cron = require("node-cron");
 const Task = require("../models/Task");
 
 cron.schedule("* * * * *", async () => {
-  // Runs every minute
   try {
     const dueTime = new Date();
+    const overdueTasks = await Task.find({
+      deadline: { $lt: dueTime },
+      status: { $ne: "pending" },
+      status: { $ne: "completed" },
+    });
 
-    // Update tasks whose deadline has passed and are not "pending" or "completed"
-    const result = await Task.updateMany(
-      {
-        deadline: { $lt: dueTime },
-        status: { $nin: ["pending", "completed"] },
-      },
-      { $set: { status: "pending" } }
-    );
-
-    if (result.modifiedCount > 0) {
-      console.log(`Updated ${result.modifiedCount} overdue tasks.`);
+    if (overdueTasks.length > 0) {
+      for (const task of overdueTasks) {
+        task.status = "pending";
+        await task.save();
+      }
     }
   } catch (error) {
     console.error("Error updating overdue tasks:", error);
