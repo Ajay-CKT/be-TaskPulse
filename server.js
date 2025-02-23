58ff2d4171cbdba37e12ba6175672ad61e632e7d
Ajay-CKT <ajayckt917@gmail.com>
2025-02-23 12:07:24 +0530

    listen for these events and refresh state

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const mongoose = require("mongoose");
-const { MONGODB_URI, PORT } = require("./utils/config");
+const { MONGODB_URI, PORT, CLIENT_URL1 } = require("./utils/config");
 const app = require("./app");
+const http = require("http");
+const { Server } = require("socket.io");
 
 mongoose
   .connect(MONGODB_URI)
   .then(() => {
     console.info("✅Database connected...");
+    const server = http.createServer(app);
+    const io = new Server(server, {
+      cors: {
+        origin: CLIENT_URL1,
+      },
+    });
     app.listen(PORT, () => console.info("🚀Server running..."));
-    require("./utils/taskScheduler");
+    require("./utils/taskScheduler")(io);
     console.info("⏳Task Scheduler started...");
   })
   .catch((error) => console.error(error));
--- utils/taskScheduler.js
+++ utils/taskScheduler.js
@@ -1,26 +1,27 @@
 const cron = require("node-cron");
 const Task = require("../models/Task");
-const io = require("socket.io");
 
-cron.schedule("* * * * *", async () => {
-  try {
-    const result = await Task.updateMany(
-      {
-        deadline: { $lt: new Date() },
-        status: { $nin: ["pending", "completed"] },
-      },
-      { $set: { status: "pending" } }
-    );
+module.exports = (io) => {
+  cron.schedule("* * * * *", async () => {
+    try {
+      const result = await Task.updateMany(
+        {
+          deadline: { $lt: new Date() },
+          status: { $nin: ["pending", "completed"] },
+        },
+        { $set: { status: "pending" } }
+      );
 
-    if (result.modifiedCount > 0) {
-      console.log(`✅ ${result.modifiedCount} tasks moved to pending status.`);
-      io.emit("taskUpdated");
-    } else {
-      console.log("✅ No overdue tasks found.");
+      if (result.modifiedCount > 0) {
+        console.log(
+          `✅ ${result.modifiedCount} tasks moved to pending status.`
+        );
+        io.emit("taskUpdated"); // Use the passed Socket.io instance to emit the event
+      } else {
+        console.log("✅ No overdue tasks found.");
+      }
+    } catch (error) {
+      console.error("❌ Error updating overdue tasks:", error);
     }
-  } catch (error) {
-    console.error("❌ Error updating overdue tasks:", error);
-  }
-});
-
-module.exports = cron;
+  });
+};
